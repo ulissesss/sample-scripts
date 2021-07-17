194b8d55f7593c9c9e907ab8b72a16f6bed9665e
NinjaAung <ninja.aung02@gmail.com>
2021-07-17 13:06:36 -0700

    cleaned up code

--- python/macos_info.py
+++ python/macos_info.py
@@ -1,40 +1,7 @@
 #!/usr/local/bin/python3
-'''
-Collect machine, OS data.
-    computer name (nodename/name of machine on network)
-    ip address
-    uptime
-    username
-    ssid (name of wifi network)
-    os
-Export the data in JSON format.
-    {
-     machine name
-     ip address
-     uptime
-     username
-     ssid
-     os
-    }
-'''
-
-import os
-import re
-import socket
-import subprocess
-from time import sleep
+import os, re, socket, subprocess,time
 
 def get_computer_name():
-    '''
-    https://docs.python.org/3/library/os.html#os.uname
-    Returns information identifying the current operating system. The return value is an object with
-    five attributes:
-    sysname - operating system name
-    nodename - name of machine on network (implementation-defined)
-    release - operating system release
-    version - operating system version
-    machine - hardware identifier
-    '''
     user_info = os.uname()
     computer_name = user_info[1]
     return computer_name
@@ -92,32 +59,23 @@ def get_serial_number():
     out = proc2.communicate()
     return str(re.findall(r'"(.*?)"', out[0].decode())[1])
 
+EXPORT_DATA = f'''
+Machine Name,   {get_computer_name()}
+Ip Address,     {get_ip_address()}
+Mac,            {get_mac(get_ip_address())}
+Public Ip,      {get_public_ip()}
+Uptime,        "{get_uptime()}"
+Username,       {get_username()}
+SSID,           {get_ssid()}
+OS Version,     {get_os_version()}
+'''
 
 def main():
     with open(get_serial_number()+".csv", "w") as f:
-        f.write(f"""Uptime, " {get_uptime()}"
-Username, {get_username()}
-Computer Name,{get_computer_name()}
-SSID,{get_ssid()}
-Ip,{get_ip_address()}
-Public Ip,{get_public_ip()}
-Mac,{get_mac(get_ip_address())}
-OS Version,{get_os_version()}
-""")
+        f.write(EXPORT_DATA)
         f.close()
-    sleep(1)
-
-    print("\n" + get_computer_name())
-    print(get_ip_address())
-    print(get_public_ip())
-    print(get_uptime())
-    print(get_username())
-    print(get_ssid())
-    print(get_os_version())
-    print(get_mac(get_ip_address()))
-    print(get_serial_number())
-    # pass
-
+    time.sleep(1)
+    # print(EXPORT_DATA)
 
 if __name__ == '__main__':
     main()
